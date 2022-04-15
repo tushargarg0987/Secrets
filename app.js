b2c4da301d3e1732b427afd7e2e522ee88e0dd9a
Tushar <tushargarg0987@gmail.com>
2022-04-15 16:26:12 +0530

    Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
+
 
 const app = express();
 
@@ -21,10 +22,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-
-
-// To encrypt the password
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema)
 
@@ -43,7 +40,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save(function(err){
@@ -58,7 +55,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req, res){
     const username = req.body.username
-    const password= req.body.password
+    const password= md5(req.body.password)
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
